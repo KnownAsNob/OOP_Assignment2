3786c328e266177d9ec6754549b0fbd6897b12ad
Niall O'Brien <obrienn68@yahoo.co.uk>
2018-04-21 21:18:11 +0100

    Shots working

--- OOP_Assignment2/Assets/Scripts/PlayerController.cs
+++ OOP_Assignment2/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,22 @@ public class PlayerController : MonoBehaviour
 	public Boundary boundary;
 	private Rigidbody rb;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
